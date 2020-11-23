ba0a4d11fce006705d866e6221679bf8a07d930d
hujinhai666 <47549127+hujinhai666@users.noreply.github.com>
2020-11-23 12:07:41 +0800

    每过约1秒钟，输出一个100以内int型的二维坐标。
    666

--- 112302.App.java
+++ 112302.App.java
@@ -2,9 +2,9 @@ package pac;
 
 public class App2 {
     public  static  void main(String[] args) throws InterruptedException {
-        while(true) { 
+        while(true) {
            Thread.sleep(1000);
-            System.out.println((int)(Math.random() * 100%100)+" , "+(int)(Math.random() * 100%100));
+            System.out.println("X:"+(int)(Math.random() * 100%100)+" , "+"Y:"+(int)(Math.random() * 100%100));
         }
     }
 }
