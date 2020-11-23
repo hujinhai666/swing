72ee61fed1a0de81fc0e5ba1b34931554b3a89a4
hujinhai666 <47549127+hujinhai666@users.noreply.github.com>
2020-11-23 12:02:55 +0800

    每过约1秒钟，输出一个int型的二维坐标。
    666

---
+++ 112302.App.java
@@ -0,0 +1,10 @@
+package pac;
+
+public class App2 {
+    public  static  void main(String[] args) throws InterruptedException {
+        while(true) {
+           Thread.sleep(1000);
+            System.out.println((int)(Math.random() * 100%100)+" , "+(int)(Math.random() * 100%100));
+        }
+    }
+}
