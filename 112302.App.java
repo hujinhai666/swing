7a0c7aef32907bc3fa8febcc09c77e33d3ec0a6a
hujinhai666 <47549127+hujinhai666@users.noreply.github.com>
2020-11-23 12:04:33 +0800

    每过约1秒钟，输出一个100以内int型的二维坐标。

--- 112302.App.java
+++ 112302.App.java
@@ -2,7 +2,7 @@ package pac;
 
 public class App2 {
     public  static  void main(String[] args) throws InterruptedException {
-        while(true) {
+        while(true) { 
            Thread.sleep(1000);
             System.out.println((int)(Math.random() * 100%100)+" , "+(int)(Math.random() * 100%100));
         }
