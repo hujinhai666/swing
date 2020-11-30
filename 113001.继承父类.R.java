a15904847f7e6e907c2677367149afd26b4cf884
hujinhai666 <47549127+hujinhai666@users.noreply.github.com>
2020-11-30 11:55:21 +0800

    113001.继承父类.R.java

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -2,11 +2,17 @@ package pac;
 
 public class R {
 	static void lx1() {
-		String boy_filePathString="img/body.gif";
-		String girl_filePathString="img/girl.gif";
-		int [][]array2d= {{1,2,3},{4,5,6}};
-		System.out.println(boy_filePathString);
-		System.out.println(girl_filePathString);
-		System.out.println(array2d[1][2]);
+		String boy="img/body.gif";
+		String girl="img/girl.gif";
+		int [][]array2d= {{1,2,3},{4,5,6},{7,8,9}};
+		for (int h = 0; h < array2d.length; h++) {
+			for (int l = 0; l < array2d[0].length; l++) {
+				System.out.print(array2d[h][l]+" ");
+			}
+			System.out.println("");
+		}
+		System.out.println(boy);
+		System.out.println(girl);
 	}
 }
+
