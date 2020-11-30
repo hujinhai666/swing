ca941e4af7daaa6b11ad0e1ca5e82e7308c2191c
hujinhai666 <47549127+hujinhai666@users.noreply.github.com>
2020-11-30 11:56:56 +0800

    113002.接口.R.java

--- 113002.接口.R.java
+++ 113002.接口.R.java
@@ -1,12 +1,17 @@
 package pac;
 
-public interface Jc {
+public interface R2 {
 	static void lx2() {
 		String boy_filePathString="img/nan.gif";
 		String girl_filePathString="img/nv.gif";
-		int [][]array2d= {{10,20,30},{40,50,60}};
+		int [][]array2d= {{10,20,30},{40,50,60},{70,80,90}};
+		for (int h = 0; h < array2d.length; h++) {
+			for (int l = 0; l < array2d[0].length; l++) {
+				System.out.print(array2d[h][l]+" ");
+			}
+			System.out.println("");
+		}
 		System.out.println(boy_filePathString);
 		System.out.println(girl_filePathString);
-		System.out.println(array2d[1][2]);
 	}
 }
