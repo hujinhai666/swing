14057c64c2bc218edaf59ec16d196abe1702237e
hujinhai666 <47549127+hujinhai666@users.noreply.github.com>
2020-11-30 11:43:05 +0800

    113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,12 @@
+package pac;
+
+public interface Jc {
+	static void lx2() {
+		String boy_filePathString="img/nan.gif";
+		String girl_filePathString="img/nv.gif";
+		int [][]array2d= {{10,20,30},{40,50,60}};
+		System.out.println(boy_filePathString);
+		System.out.println(girl_filePathString);
+		System.out.println(array2d[1][2]);
+	}
+}
